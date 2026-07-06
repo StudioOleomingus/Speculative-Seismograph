7eae896d0ebe1ad2a449a2d88d06b497e4a4d618
Dhruv Jani <67113736+StudioOleomingus@users.noreply.github.com>
2026-07-06 14:23:36 +0530

    Revert "Mobile Update"
    This reverts commit af90e96217b154e4c422f8fa75456615f50b8109.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ import { drawMarkdown } from './text-renderer.js';
 import { initScene, resetScroll } from './scene.js';
 import { initToggles } from './toggles.js';
 import { initPen } from './pen.js';
-import { initMobile } from './mobile.js';
 
 let manifest = null;
 
@@ -70,7 +69,6 @@ async function loadStory(bits) {
   }
 }
 
-initMobile(); // must run before initScene so boosted scroll config applies
 initScene(document.getElementById('canvas-container'));
 initPen();
 
